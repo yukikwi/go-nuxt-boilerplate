8ac888a448056af0ddc5968ac8cdddec50ea7d8c
Pachara Chantawong <66879660+yukikwi@users.noreply.github.com>
2025-11-02 22:17:55 +0700

    feat: expose db connection

--- server/src/config/db.go
+++ server/src/config/db.go
@@ -10,8 +10,10 @@ import (
 func OpenDatabaseConnection() *gorm.DB {
 	var db, err = gorm.Open(postgres.Open(Config["Db"]), &gorm.Config{})
 	if err != nil {
-		slog.Error("failed to connect database", err)
+		slog.Error("failed to connect database", "error", err)
 	}
 
 	return db
 }
+
+var DB = OpenDatabaseConnection()
