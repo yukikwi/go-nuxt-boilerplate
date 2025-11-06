f4edcd332948b3f9849391dd3737372884c3ea17
Pachara Chantawong <66879660+yukikwi@users.noreply.github.com>
2025-11-06 23:56:26 +0700

    feat: load env from .env on utils/env init

--- server/src/utils/env.go
+++ server/src/utils/env.go
@@ -1,6 +1,19 @@
 package utils
 
-import "os"
+import (
+	"log/slog"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		slog.Error("Error loading .env file", "error", err)
+	}
+	slog.Info("Loaded env from .env")
+}
 
 func GetEnv(key string, fallback string) string {
 	if value, exists := os.LookupEnv(key); exists {
