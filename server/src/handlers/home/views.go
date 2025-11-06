0d6e91c7372b4f23a660cbfaf3d83719d597d2c9
Pachara Chantawong <66879660+yukikwi@users.noreply.github.com>
2025-11-06 23:56:48 +0700

    feat: insert message to database

--- server/src/handlers/home/views.go
+++ server/src/handlers/home/views.go
@@ -4,6 +4,8 @@ import (
 	"log/slog"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/yukikwi/go-nuxt-boilerplate/config"
+	"github.com/yukikwi/go-nuxt-boilerplate/models"
 	"github.com/yukikwi/go-nuxt-boilerplate/utils"
 )
 
@@ -12,6 +14,14 @@ func SpeakView(c *fiber.Ctx) error {
 	if err := utils.ValidateDataStruct(c, &body); err != nil {
 		slog.Error("homeRouter.Post('/speak')", "error", err)
 		return c.JSON(utils.ValidationErrorResponse{Error: err.Error()})
+	}
+
+	// Insert Message to Model
+	message := models.Message{MessageText: body.Message}
+	result := config.Db.Create(&message)
+	if result.Error != nil {
+		slog.Error("Failed to insert message", "error", result.Error)
+		return c.Status(fiber.StatusInternalServerError).JSON(utils.GenericErrorResponse{Error: "Failed to save message"})
 	}
 
 	return c.JSON(SpeakResponseSerializer{Result: "You said: " + body.Message})
