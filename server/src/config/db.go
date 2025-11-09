b013a36a77d898806a56d025a2be6bc64be18a51
Pachara Chantawong <66879660+yukikwi@users.noreply.github.com>
2025-11-09 14:14:56 +0700

    fix: correct database connection log

--- server/src/config/db.go
+++ server/src/config/db.go
@@ -12,7 +12,9 @@ var Db *gorm.DB
 func OpenDatabaseConnection() *gorm.DB {
 	var db, err = gorm.Open(postgres.Open(Config.Db), &gorm.Config{})
 	if err != nil {
-		slog.Error("failed to connect database", "error", err)
+		slog.Error("Failed to connect database", "error", err)
+	} else {
+		slog.Info("Database connected.")
 	}
 
 	return db
@@ -21,5 +23,4 @@ func OpenDatabaseConnection() *gorm.DB {
 func init() {
 	slog.Info("Connecting to database...")
 	Db = OpenDatabaseConnection()
-	slog.Info("Database connected.")
 }
