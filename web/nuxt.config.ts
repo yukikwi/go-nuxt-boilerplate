4622b40739d3cf3b0a98bb52b0e66b33c05ed4a5
Pachara Chantawong <66879660+yukikwi@users.noreply.github.com>
2025-11-12 15:11:41 +0700

    fix: kubb config and generated file type error

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -2,7 +2,26 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  features: {
+    devLogs: true
+  },
   experimental: {
     entryImportMap: false
+  },
+  runtimeConfig: {
+    public: {
+      apiBase: 'http://localhost:8080',
+    },
+  },
+  typescript: {
+    tsConfig: {
+      compilerOptions: {
+        noEmit: true,
+        allowImportingTsExtensions: true,
+      }
+    },
+    nodeTsConfig: {
+      include: ['../kubb.config.ts']
+    }
   }
 })
